b0e08075b01a06599204c82cdfa0b89499b48fff
Patrick Boucher <pboucher@patrickboucher.com>
2013-08-23 13:27:58 -0400

    #22548 - No need for manual OTL registration anymore

--- app.py
+++ app.py
@@ -12,22 +12,8 @@
 Alembic Output App for Houdini
 """
 
-import os
 import sgtk
-
-import hou
 
 
 class AlembicOutputNode(sgtk.platform.Application):
-
-    def init_app(self):
-        """
-        Called as the application is being initialized
-        """
-
-        # Register all our OTL files with the Houdini engine
-        otl_path = os.path.join(self.disk_location, 'otls')
-        for filename in os.listdir(otl_path):
-            if os.path.splitext(filename)[-1] == '.otl':
-                path = os.path.join(otl_path, filename)
-                self.engine.register_otl(path)
+    pass
