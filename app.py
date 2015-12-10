73b5fa391a8e5eb4baaf83cec4c6d24c3d092486
Josh Tomlinson <josh.tomlinson@shotgunsoftware.com>
2015-12-10 11:21:02 -0500

    #31683 Merged updates from Pixomondo, cleanup, refactor
    Merges in changes provided by Pixomondo (thanks guys!!). Now includes the
    ability to define multiple output profiles in the configuration. Includes
    methods for converting to and from regular, built-in Alembic nodes. Overall
    cleanup and refactoring of the code to bring it more in line with other
    toolkit apps (like nuke write node).
    
    Closes #2.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 Shotgun Software Inc.
+# Copyright (c) 2015 Shotgun Software Inc.
 # 
 # CONFIDENTIAL AND PROPRIETARY
 # 
@@ -9,11 +9,62 @@
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 """
-Alembic Output App for Houdini
+Alembic Output node App for use with Toolkit's Houdini engine.
 """
 
 import sgtk
 
 
-class AlembicOutputNode(sgtk.platform.Application):
-    pass
+class TkAlembicNodeApp(sgtk.platform.Application):
+    """The Alembic Output Node."""
+
+    def init_app(self):
+        """Initialize the app."""
+
+        tk_houdini_alembic = self.import_module("tk_houdini_alembicnode")
+        self.handler = tk_houdini_alembic.TkAlembicNodeHandler(self)
+
+    def convert_to_regular_alembic_nodes(self):
+        """Convert Toolkit Alembic nodes to regular Alembic nodes.
+        
+        Convert all Toolkit Alembic nodes found in the current script to
+        regular Alembic nodes. Additional Toolkit information will be stored in
+        user data named 'tk_*'
+
+        Example usage::
+
+        >>> import sgtk
+        >>> eng = sgtk.platform.current_engine()
+        >>> app = eng.apps["tk-houdini-alembicnode"]
+        >>> app.convert_to_regular_alembic_nodes()
+
+        """
+
+        self.log_debug(
+            "Converting Toolkit Alembic nodes to built-in Alembic nodes.")
+        tk_houdini_alembic = self.import_module("tk_houdini_alembicnode")
+        tk_houdini_alembic.TkAlembicNodeHandler.\
+            convert_to_regular_alembic_nodes(self)
+
+    def convert_back_to_tk_alembic_nodes(self):
+        """Convert regular Alembic nodes back to Tooklit Alembic nodes.
+        
+        Convert any regular Alembic nodes that were previously converted
+        from Tooklit Alembic nodes back into Toolkit Alembic nodes.
+
+        Example usage::
+
+        >>> import sgtk
+        >>> eng = sgtk.platform.current_engine()
+        >>> app = eng.apps["tk-houdini-alembicnode"]
+        >>> app.convert_back_to_tk_alembic_nodes()
+
+        """
+
+        self.log_debug(
+            "Converting built-in Alembic nodes back to Toolkit Alembic nodes.")
+        tk_houdini_alembic = self.import_module("tk_houdini_alembicnode")
+        tk_houdini_alembic.TkAlembicNodeHandler.\
+            convert_back_to_tk_alembic_nodes(self)
+
+
