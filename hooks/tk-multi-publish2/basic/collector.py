e3734f4df945202a8ce6b7142fd185188b4b594f
Bo Kamphues <b.kamphues@outlook.com>
2020-08-26 15:41:13 +0200

    Updated collect_tk_houdini_cachenodes()

--- hooks/tk-multi-publish2/basic/collector.py
+++ hooks/tk-multi-publish2/basic/collector.py
@@ -26,7 +26,7 @@ class TKHoudiniCacheNodeCollector(HookBaseClass):
         """
 
         # process all other items of houdini first
-        super().process_current_session()
+        super().process_current_session(settings, parent_item)
 
         self.collect_tk_houdini_cachenodes()
 
@@ -34,3 +34,27 @@ class TKHoudiniCacheNodeCollector(HookBaseClass):
         """
         Collect all instances of the tk-houdini-cachenodes.
         """
+
+        publisher = self.parent
+        engine = publisher.engine
+        app = engine.apps.get("tk-houdini-cachenode")
+
+        if not app:
+            self.logger.debug("tk-houdini-cachenode does not seem to be installed")
+            return
+
+        try:
+            nodes = app.get_nodes()
+        except:
+            self.logger.warning("could not get any instances of tk-houdini-cachenode")
+
+        work_template = app.get_work_file_template()
+
+        for node in nodes:
+            out_path = app.get_output_path(node)
+
+            if not os.path.exists(out_path):
+                continue
+            
+            self.logger.info("processing sgtk_cachenode: %s" % (node.path()))
+            
